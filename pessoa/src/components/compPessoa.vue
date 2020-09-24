bc2022a824246ba9dca9ab5811e000e6d3d2833e
voyane rogerio <voyanerogerio@gmail.com>
2020-09-24 13:07:12 -0700

    Alterado o conteudo do arquivo compPessoa

--- pessoa/src/components/compPessoa.vue
+++ pessoa/src/components/compPessoa.vue
@@ -1,21 +1,25 @@
 <template>
     <div> 
         <h2> <strong> Cadastro Pessoa </strong></h2>
-    <p>        <input v-model="nome" />
+    <p> <input v-model="nome" />
         <button v-on:click= "inserir"> NOME </button> </p>
-       
+        
+         <div>
+             
+        </div>    
     </div>
+   
 </template>
 <script>
 export default {
     data: function () { 
         return {
-            nome: ''
+            nomes: ''
         }
     },
     methods: {
         inserir: function (){
-            this.nome = ''
+            this.nome.push(this.nome)
         }
     }
 }
