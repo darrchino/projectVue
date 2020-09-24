ad8ea587879842c0423396111d7c7271c71207ce
voyane rogerio <voyanerogerio@gmail.com>
2020-09-24 00:19:02 -0700

    Adicionado a componente pessoa no projecto

---
+++ pessoa/src/components/compPessoa.vue
@@ -0,0 +1,20 @@
+<template>
+    <div> 
+        <input v-model="nome" />
+        <button v-on: click="inserir"> Click </button>
+    </div>
+</template>
+<script>
+export default {
+    data: function () { 
+        return {
+            nome: '',
+        }
+    },
+    methods: {
+        inserir : function (){
+            this.nome = ''
+        }
+    }
+}
+</script>
