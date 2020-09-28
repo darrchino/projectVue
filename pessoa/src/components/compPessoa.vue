e148012a59ed54122c4ed1feb8c8445517e49f9c
voyane rogerio <voyanerogerio@gmail.com>
2020-09-27 23:11:37 -0700

    Modificado o cadastro Pessoa

--- pessoa/src/components/compPessoa.vue
+++ pessoa/src/components/compPessoa.vue
@@ -3,10 +3,19 @@
         <h2> <strong> Cadastro Pessoa </strong></h2>
     <p> <input v-model="nome" />
         <button v-on:click= "inserir"> NOME </button> </p>
+        <p> {{nome}} </p>
         
-         <div>
-             
-        </div>    
+        <p> <input v-model="idade" />
+        <button v-on:click= "inserir"> IDADE </button> </p>
+        <p> {{idade}} </p>
+
+        <p> <input v-model="residencia" />
+        <button v-on:click= "inserir"> RESIDENCIA </button> </p>
+        <p> {{residencia}} </p>
+
+        <p> <input v-model="estado_civil" />
+        <button v-on:click= "inserir"> ESTADO </button> </p>
+        <p> {{estado_civil}} </p>
     </div>
    
 </template>
@@ -14,12 +23,19 @@
 export default {
     data: function () { 
         return {
-            nomes: ''
+            nome: '',
+            idade: '',
+            residencia: '',
+            estado_civil: ''
         }
     },
     methods: {
         inserir: function (){
-            this.nome.push(this.nome)
+            this.nome = ''
+            this.idade=''
+            this.residencia= ''
+            this.estado_civil=''
+            
         }
     }
 }
